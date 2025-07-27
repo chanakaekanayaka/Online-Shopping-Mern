12a9828f6daef89d77ba101e5bfa3c12f1945a38
chanakaekanayaka <chanakaekanayaka15@gmail.com>
2025-07-27 11:44:10 +0530

    create product model ⏫

---
+++ models/product.js
@@ -0,0 +1,53 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    productID : {
+        type : String,
+        required : true,
+        uniqe : true
+    },
+    productName : {
+        type : String,
+        required : true
+    },
+    altNames : {
+        type : [String],
+        default : []
+    },
+    labelledPrice : {
+        type : Number,
+        required : true
+    },
+    price : {
+        type : Number,
+        required : true
+    },
+    image : {
+        type : [String],
+        default : ["/default-product.jpg"]
+    },
+    description : {
+        trpe : String,
+        required : true
+    },
+    stock : {
+        type : Number,
+        required : true,
+        default : 0
+    },
+    isAvilable : {
+        type : Boolean,
+        default : true
+    },
+    category : {
+        type : String,
+        required : true,
+        default : "cosmatic"
+    }
+
+
+})
+
+const Product = mongoose.model("products",productSchema)
+
+export default Product;
